970abc89597615eb2e0ef3b74e9e5450cb0fda9a
pknoetze <46191984+pknoetze@users.noreply.github.com>
2025-01-25 18:55:34 +0000

    Initial commit from your app

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/trading-buddy/', // Update this to match your repository name
   test: {
     globals: true,
     environment: 'jsdom',
